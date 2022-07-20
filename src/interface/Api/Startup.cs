ece1bea1bebb5a5d79fdfe1ef7bc05b0155ff560
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-20 20:43:50 -0300

    Create Api project

---
+++ src/interface/Api/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api;
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddApiVersioning(options =>
+        {
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ReportApiVersions = true;
+        });
+
+        services.AddVersionedApiExplorer(setup =>
+        {
+            setup.GroupNameFormat = "'v'VVV";
+            setup.SubstituteApiVersionInUrl = true;
+        });
+
+        services.ConfigureOptions<ConfigureSwaggerOptions>();
+        services.AddSwaggerGen().AddSwaggerGenNewtonsoftSupport();
+        services.AddRouting(options => options.LowercaseUrls = true);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSwagger();
+        app.UseApiVersioning();
+        app.UseSwaggerUI(c => { c.SwaggerEndpoint("v1/swagger.json", "v1"); });
+
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
