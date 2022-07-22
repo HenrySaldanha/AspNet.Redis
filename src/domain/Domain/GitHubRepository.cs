729b0f7a9e64b4f631051869c2ce4302800b7fda
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-07-22 19:15:18 -0300

    Created Owner entity for github response

--- src/domain/Domain/GitHubRepository.cs
+++ src/domain/Domain/GitHubRepository.cs
@@ -2,11 +2,19 @@
 {
     public class GitHubRepository
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
         public string Language { get; set; }
         public uint Stars { get; set; }
         public DateTime CreateTime { get; set; }
+        public RepositoryOwner Owner { get; set; }
+    }
+    public class RepositoryOwner
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string LoginName { get; set; }
     }
 }
--- src/interface/Api/Models/Response/GitHubRepositoryResponse.cs
+++ src/interface/Api/Models/Response/GitHubRepositoryResponse.cs
@@ -1,30 +1,52 @@
 ﻿using Domain;
 
-namespace Api.Models.Response
+namespace Api.Models.Response;
+
+public class GitHubRepositoryResponse
 {
-    public class GitHubRepositoryResponse
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public string Language { get; set; }
+    public uint Stars { get; set; }
+    public DateTime CreateTime { get; set; }
+    public RepositoryOwnerResponse Owner { get; set; }
+
+    public static implicit operator GitHubRepositoryResponse(GitHubRepository repo)
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
-        public string Language { get; set; }
-        public uint Stars { get; set; }
-        public DateTime CreateTime { get; set; }
+        if (repo is null)
+            return null;
 
-        public static implicit operator GitHubRepositoryResponse(GitHubRepository repo)
+        return new GitHubRepositoryResponse
         {
-            if (repo is null)
-                return null;
+            CreateTime = repo.CreateTime,
+            Name = repo.Name,
+            Description = repo.Description,
+            Url = repo.Url,
+            Language = repo.Language,
+            Stars = repo.Stars,
+            Id = repo.Id,
+            Owner = (RepositoryOwnerResponse)repo.Owner,
+        };
+    }
+}
+public class RepositoryOwnerResponse
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string LoginName { get; set; }
 
-            return new GitHubRepositoryResponse
-            {
-                CreateTime = repo.CreateTime,
-                Name = repo.Name,
-                Description = repo.Description,
-                Url = repo.Url,
-                Language = repo.Language,
-                Stars = repo.Stars,
-            };
-        }
+    public static implicit operator RepositoryOwnerResponse(RepositoryOwner repo)
+    {
+        if (repo is null)
+            return null;
+
+        return new RepositoryOwnerResponse
+        {
+            Id = repo.Id,
+            LoginName = repo.LoginName,
+            Url = repo.Url
+        };
     }
 }
