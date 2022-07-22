9aea96b08185ea8ae941402cfc8f1ac3bb65d00c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-21 23:18:24 -0300

    Add new keys

--- src/application/Application/Helpers/KeysHelper.cs
+++ src/application/Application/Helpers/KeysHelper.cs
@@ -1,8 +1,7 @@
 ﻿namespace Application.Helpers;
 public static class KeysHelper
 {
-    public static string GetFibonacciKey(uint n)
-    {
-        return $"FIBONACCI:{n}";
-    }
+    public static string GetFibonacciKey(uint n) => $"FIBONACCI:{n}";
+    public static string GetLatestRepositoriesKey(string userName) => $"GITHUB_LATEST_REPOS:{userName}";
+    public static string GetMostStarredRepositoryKey(string userName) => $"GITHUB_MOST_STAR_REPOS:{userName}";
 }
