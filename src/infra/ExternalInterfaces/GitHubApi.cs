0127ee2f1041320115ed28e5c74c05f9f73bd202
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-07-22 19:14:37 -0300

    Moved External Interfaces project

---
+++ src/infra/ExternalInterfaces/GitHubApi.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using ExternalInterfaces.Model;
+using Flurl.Http;
+using Serilog;
+using System.Net;
+using System.Text.Json;
+
+namespace ExternalInterfaces;
+public class GitHubApi
+{
+    public async Task<IEnumerable<GitHubRepository>> GetRepositoriesByUser(string userName)
+    {
+        Log.Debug("Service: {0} Method: {1} Request: {2}",
+            nameof(GitHubApi), nameof(GetRepositoriesByUser), userName);
+
+        var url = $"https://api.github.com/users/{userName}/repos";
+        try
+        {
+            var apiResponse = await url
+                .WithHeader("User-Agent", "MyRedisApp")
+                .AllowAnyHttpStatus()
+                .GetAsync();
+
+            if(apiResponse.StatusCode == (int)HttpStatusCode.OK)
+            {
+                var repos = await apiResponse.GetJsonAsync<IEnumerable<ApiRepositoryResponse>>();
+                return repos.Select(c => (GitHubRepository)c);
+            }
+
+            return Enumerable.Empty<GitHubRepository>();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "Error: {0}", ex.Message);
+            throw;
+        }
+    }
+}
---
+++ src/infra/ExternalInterfaces/Model/ApiRepositoryResponse.cs
@@ -0,0 +1,72 @@
+﻿using Domain;
+using System.Text.Json.Serialization;
+
+namespace ExternalInterfaces.Model;
+
+public class ApiRepositoryResponse
+{
+    //[JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    //[JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    //[JsonPropertyName("owner")]
+    public ApiOwnerResponse Owner { get; set; }
+
+    //[JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    //[JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    //[JsonPropertyName("created_at")]
+    public DateTime Created_at { get; set; }
+
+    //[JsonPropertyName("stargazers_count")]
+    public int Stargazers_count { get; set; }
+
+    //[JsonPropertyName("language")]
+    public string Language { get; set; }
+
+    public static implicit operator GitHubRepository(ApiRepositoryResponse apiModel)
+    {
+        if (apiModel is null)
+            return null;
+
+        return new GitHubRepository
+        {
+            CreateTime = apiModel.Created_at,
+            Id = apiModel.Id,
+            Description = apiModel.Description,
+            Language = apiModel.Language,
+            Name = apiModel.Name,
+            Stars = (uint)apiModel.Stargazers_count,
+            Url = apiModel.Url,
+            Owner = (RepositoryOwner)apiModel.Owner
+        };
+    }
+}
+
+public class ApiOwnerResponse
+{
+    //[JsonPropertyName("login")]
+    public string Login { get; set; }
+    //[JsonPropertyName("id")]
+    public int Id { get; set; }
+    //[JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    public static implicit operator RepositoryOwner(ApiOwnerResponse owner)
+    {
+        if (owner is null)
+            return null;
+
+        return new RepositoryOwner
+        {
+            Id = owner.Id,
+            Url = owner.Url,
+            LoginName = owner.Login
+        };
+    }
+}
