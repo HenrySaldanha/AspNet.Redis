a072b8fe44e97fddbe8cd71ac16e3ec11f114c57
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-22 19:19:44 -0300

    namespace top level

--- src/domain/Domain/GitHubRepository.cs
+++ src/domain/Domain/GitHubRepository.cs
@@ -1,20 +1,18 @@
-﻿namespace Domain
+﻿namespace Domain;
+public class GitHubRepository
 {
-    public class GitHubRepository
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Url { get; set; }
-        public string Language { get; set; }
-        public uint Stars { get; set; }
-        public DateTime CreateTime { get; set; }
-        public RepositoryOwner Owner { get; set; }
-    }
-    public class RepositoryOwner
-    {
-        public int Id { get; set; }
-        public string Url { get; set; }
-        public string LoginName { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+    public string Language { get; set; }
+    public uint Stars { get; set; }
+    public DateTime CreateTime { get; set; }
+    public RepositoryOwner Owner { get; set; }
+}
+public class RepositoryOwner
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string LoginName { get; set; }
 }
