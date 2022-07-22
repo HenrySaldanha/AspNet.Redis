567352620bb7278a7d7e0c8c87862cacf466dc0a
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-21 23:19:36 -0300

    Add domain project

---
+++ src/domain/Domain/GitHubRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class GitHubRepository
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Language { get; set; }
+        public uint Stars { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
