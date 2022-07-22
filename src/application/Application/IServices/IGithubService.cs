6370591bf90fd9831a98940db032d9b11fab72b7
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-21 23:19:16 -0300

    Add new service

---
+++ src/application/Application/IServices/IGithubService.cs
@@ -0,0 +1,8 @@
+﻿using Domain;
+
+namespace Application.IServices;
+public interface IGithubService
+{
+    public Task<IEnumerable<GitHubRepository>> GetLatestRepositoriesAsync(string userName);
+    public Task<IEnumerable<GitHubRepository>> GetMostStarredRepositoryAsync(string userName);
+}
---
+++ src/application/Application/Services/GithubService.cs
@@ -0,0 +1,53 @@
+﻿using Application.Helpers;
+using Application.IServices;
+using Domain;
+using ExternalInterfaces;
+using Repository.Cache;
+using Serilog;
+
+namespace Application.Services;
+public class GithubService : IGithubService
+{
+    private readonly ICacheRepository _cache;
+    private readonly GitHubApi _gitHubApi;
+
+    public GithubService(ICacheRepository cache)
+    {
+        _cache = cache;
+        _gitHubApi = new GitHubApi();
+    }
+
+    public async Task<IEnumerable<GitHubRepository>> GetLatestRepositoriesAsync(string userName)
+    {
+        Log.Information("Service: {0} Method: {1} Request: {2}",
+            nameof(GithubService), nameof(GetLatestRepositoriesAsync), userName);
+
+        var key = KeysHelper.GetLatestRepositoriesKey(userName);
+        var cacheData = await _cache.GetAsync<IEnumerable<GitHubRepository>>(key);
+
+        if (cacheData is not null)
+            return cacheData;
+
+        var repositories = _gitHubApi.GetRepositoriesByUser(userName);
+
+
+        //TODO: implement this
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<GitHubRepository>> GetMostStarredRepositoryAsync(string userName)
+    {
+        Log.Information("Service: {0} Method: {1} Request: {2}",
+          nameof(GithubService), nameof(GetMostStarredRepositoryAsync), userName);
+
+        var key = KeysHelper.GetMostStarredRepositoryKey(userName);
+
+        var cacheData = await _cache.GetAsync<IEnumerable<GitHubRepository>>(key);
+
+        if (cacheData is not null)
+            return cacheData;
+
+        //TODO: implement this
+        throw new NotImplementedException();
+    }
+}
