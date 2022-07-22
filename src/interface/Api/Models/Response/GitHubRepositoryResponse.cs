24b6dc13fe2aa154c80cda82d26baace84c99997
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-21 23:21:29 -0300

    Add new project for external interfaces

---
+++ src/interface/Api/Models/Response/GitHubRepositoryResponse.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+
+namespace Api.Models.Response
+{
+    public class GitHubRepositoryResponse
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Language { get; set; }
+        public uint Stars { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public static implicit operator GitHubRepositoryResponse(GitHubRepository repo)
+        {
+            if (repo is null)
+                return null;
+
+            return new GitHubRepositoryResponse
+            {
+                CreateTime = repo.CreateTime,
+                Name = repo.Name,
+                Description = repo.Description,
+                Url = repo.Url,
+                Language = repo.Language,
+                Stars = repo.Stars,
+            };
+        }
+    }
+}
