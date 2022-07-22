41aff7e262d4ee0ef00e521f2d12a397eb34c5bf
HenrySaldanha <henry.l.saldanha@gmail.com>
2022-07-22 19:13:07 -0300

    Rename method

--- src/application/Application/IServices/IIndicatorsService.cs
+++ src/application/Application/IServices/IIndicatorsService.cs
@@ -1,7 +1,7 @@
 ﻿namespace Application.IServices;
 public interface IIndicatorsService
 {
-    public Task<(string, string)> CreateAsync(string key, string value);
+    public Task<(string, string)> InsertOrUpdateAsync(string key, string value);
     public Task<string> GetAsync(string key);
     public Task DeleteAsync(string key);
 }
