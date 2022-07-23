d5d340b2358eea93f1cbebacf2d3ab38855f4a0f
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-23 19:42:37 -0300

    feat: add health check

--- src/interface/Api/Startup.cs
+++ src/interface/Api/Startup.cs
@@ -1,7 +1,12 @@
 ﻿using DependencyInjection;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System.Diagnostics.CodeAnalysis;
+using System.Net.Mime;
 using System.Reflection;
+using System.Text.Json;
 
 namespace Api;
 
@@ -32,6 +37,7 @@ public class Startup
             setup.SubstituteApiVersionInUrl = true;
         });
 
+        //add this in dependency injection
         services.ConfigureOptions<ConfigureSwaggerOptions>();
         services.AddSwaggerGen(c =>
         {
@@ -39,6 +45,15 @@ public class Startup
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
             c.IncludeXmlComments(xmlPath);
         }).AddSwaggerGenNewtonsoftSupport();
+
+        //add this in dependency injection
+        services.AddHealthChecks()
+            .AddRedis(Configuration.GetConnectionString("Redis"), name: "redis");
+
+        //add this in dependency injection
+        services.AddHealthChecksUI()
+            .AddInMemoryStorage();
+
         services.AddRouting(options => options.LowercaseUrls = true);
         services.RegisterRedis(Configuration);
         services.RegisterOptions(Configuration);
@@ -51,6 +66,40 @@ public class Startup
         app.UseSwagger();
         app.UseApiVersioning();
         app.UseSwaggerUI(c => { c.SwaggerEndpoint("v1/swagger.json", "v1"); });
+
+        //add this in dependency injection
+        app.UseHealthChecks("/health-check",
+            new HealthCheckOptions
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    var result = JsonSerializer.Serialize(
+                    new
+                    {
+                        currentTime = DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss"),
+                        statusApplication = report.Status.ToString(),
+                        healthChecks = report.Entries.Select(c=> new
+                        {
+                            check = c.Key,
+                            status = Enum.GetName(typeof(HealthStatus),c.Value.Status)
+                        })
+                    });
+
+                    context.Response.ContentType = MediaTypeNames.Application.Json;
+                    await context.Response.WriteAsync(result);
+                }
+            });
+
+        //add this in dependency injection
+        app.UseHealthChecks("/healthchecks-data-ui", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+
+        //add this in dependency injection
+        app.UseHealthChecksUI(o => o.UIPath = "/monitor");
+
 
         if (env.IsDevelopment())
             app.UseDeveloperExceptionPage();
