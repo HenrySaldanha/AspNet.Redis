2aeb5cb1ed4c23ff5f429a4b71e3e75486153e6c
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-26 19:31:11 -0300

    refact files

--- src/application/Application/Helpers/KeysHelper.cs
+++ src/application/Application/Helpers/KeysHelper.cs
@@ -1,7 +1,10 @@
 ﻿namespace Application.Helpers;
 public static class KeysHelper
 {
-    public static string GetFibonacciKey(uint n) => $"FIBONACCI:{n}";
-    public static string GetLatestRepositoriesKey(string userName) => $"GITHUB_LATEST_REPOS:{userName}";
-    public static string GetMostStarredRepositoryKey(string userName) => $"GITHUB_MOST_STAR_REPOS:{userName}";
+    public static string GetFibonacciKey(uint n) => 
+        $"FIBONACCI:{n}";
+    public static string GetLatestRepositoriesKey(string userName) => 
+        $"GITHUB_LATEST_REPOS:{userName}";
+    public static string GetMostStarredRepositoryKey(string userName) =>
+        $"GITHUB_MOST_STAR_REPOS:{userName}";
 }
